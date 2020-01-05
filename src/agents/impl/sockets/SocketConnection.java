c9768ddd4adb229eeaa3f51de0e33dc7b61ec11e
Aidan Crooks <sir.meepington.1@gmail.com>
2020-01-05 22:52:43 +0000

    Move to impl package

--- src/agents/impl/sockets/SocketConnection.java
+++ src/agents/impl/sockets/SocketConnection.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package agents.sockets;
+package agents.impl.sockets;
 
 import java.net.Socket;
 import java.util.Objects;
